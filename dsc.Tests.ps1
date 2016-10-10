9098d2288af4cd6673b6f0cc5cad45470ee671db
David <me@david-obrien.net>
2016-10-11 08:09:41 +1100

    Fix Pester tests.

--- dsc.Tests.ps1
+++ dsc.Tests.ps1
@@ -10,11 +10,11 @@ Describe "dsc" {
         It 'tests if PSDesiredStateConfiguration module is available' {
             Get-Module -Name PSDesiredStateConfiguration | Should be $true
         }
-        It 'tests if cChoco resource is available' {
-            Get-DscResource -Name cChoco | Should be $true
+        It 'tests if cChocoInstaller resource is available' {
+            Get-DscResource -Name cChocoInstaller | Should be $true
         }
-        It 'test if xNetworking resource is available' {
-            Get-DscResource -Name xNetworking | Should be $true
+        It 'test if xFirewall resource is available' {
+            Get-DscResource -Name xFirewall | Should be $true
         }
     }
     Context 'DSC compilation tests' {
