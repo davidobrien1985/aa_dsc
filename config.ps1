f6def0de4d51c5ce51cb5ae9a2be459c9357ae75
David <me@david-obrien.net>
2016-10-11 07:18:24 +1100

    Add initial DSC config

---
+++ config.ps1
@@ -0,0 +1,31 @@
+﻿Configuration trivialweb 
+{ 
+    Import-DscResource -ModuleName cChoco 
+    Import-DscResource -ModuleName xNetworking
+
+    Node "isvbox" {   
+
+        cChocoInstaller installChoco 
+        { 
+            InstallDir = "C:\choco" 
+        }
+
+        WindowsFeature installIIS 
+        { 
+            Ensure="Present" 
+            Name="Web-Server" 
+        }
+
+        xFirewall WebFirewallRule 
+        { 
+            Direction = "Inbound" 
+            Name = "Web-Server-TCP-In" 
+            DisplayName = "Web Server (TCP-In)" 
+            Enabled = "True"
+            Action = "Allow" 
+            Protocol = "TCP" 
+            LocalPort = "80" 
+            Ensure = "Present" 
+        }
+    }    
+}
